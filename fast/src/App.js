2e1f039636cddc7d4c1d561a05753fbfed10bcd0
unknown <akashjais1705@gmail.com>
2023-09-28 22:20:40 +0530

    done2

--- fast/src/App.js
+++ fast/src/App.js
@@ -2,12 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/Navbar';
 import { MainRoutes } from './components/MainRoutes';
+import Logo3 from "./Logo3.png"
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <MainRoutes/>
+
+      <div style={{display:"flex",justifyContent:"center",gap:"100px",backgroundColor:"black"}} >
+        <div style={{marginRight:"100px"}}>
+        <Link to={"/"}  style={{textDecoration:"none"}}  >  
+      <div >
+      <img src={Logo3} width={"150px"} style={{marginRight:"60px"}}  />
+      <h5 style={{color:"white",textDecoration:"none",margin:"0"}} >REAL DESIGNS BY REAL ARTIST </h5>
+<h5 style={{color:"white",marginRight:"80px"}} >FOR REAL PEOPLE</h5>
+      </div>
+      </Link>
+      
+        </div>
+
+        <div>
+          <h6 style={{color:"white"}}  >PRODUCT</h6>
+         <p  > <Link style={{fontSize:'13px' ,textDecoration:"none",color:"lightGray"}}>Jackets</Link> </p>
+         <p  > <Link style={{fontSize:'13px',textDecoration:"none", color:"lightGray"}} >Shirt</Link>  </p>
+         <p >  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >Dresses</Link>  </p>
+         <p  >  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >Outwear</Link>  </p>
+         <p  >  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >Bottoms</Link>  </p>
+
+        </div>
+
+        <div>
+          <h6 style={{color:"white"}}  >BUYING</h6>
+         <p> <Link style={{fontSize:'13px' ,textDecoration:"none",    color:"lightGray"}}  >Shop</Link> </p>
+         <p> <Link style={{fontSize:'13px',textDecoration:"none", color:"lightGray"}} >Terms of Use</Link>  </p>
+         <p>  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >Privacy</Link>  </p>
+         <p>  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >How it Works</Link>  </p>
+         <p>  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >Custumor Service</Link>  </p>
+
+        </div>
+
+        <div>
+          <h6 style={{color:"white"}} >SOCIAL</h6>
+         <p> <Link style={{fontSize:'13px' ,textDecoration:"none",    color:"lightGray"}}  >Instagram</Link> </p>
+         <p> <Link style={{fontSize:'13px',textDecoration:"none", color:"lightGray"}} >Facebook</Link>  </p>
+         <p>  <Link style={{fontSize:'13px' ,textDecoration:"none", color:"lightGray"}} >Twitter</Link>  </p>
+        </div>
+
+
+        <div>
+          <h6  style={{color:"white",marginRight:"120px"}} >JOIN OUR COMMUNITY</h6>
+         <input type='Email'  placeholder='EMAIL ADDRESS' style={{borderRadius:"20px",width:"250px",height:"30px",backgroundColor:"black"}}/>
+        
+        </div>
+
+      </div>
+
+
     </div>
   );
 }
--- fast/src/components/Navbar.jsx
+++ fast/src/components/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import Logo3 from "../Logo3.png"
+import Fast_Fashion_logo2 from "../Fast_Fashion_logo2.png"
 export const Navbar = () => {
+
+  
+
   return (
-    <div style={{display:"flex",flexDirection:"flexStart", gap:"30px", marginLeft:"50px"}} >
-        <Link to={"/"} >Home</Link>
-        <Link to={"/about"} >About</Link>
-        <Link to={"/men"} >Men</Link>
-        <Link to={"/women"} >Women</Link>
-        <Link to={"/shop"} >Shop</Link>
-        <Link to={"/contact"} >Contact</Link>
-        
+    <div style={{display:"flex", alignItems:"center", flexDirection:"flexStart",   border:"1px solid red",width:"99%" ,margin:"auto",height:"100px" }}  >
+      <div>
+       <Link to={"/"}  >  
+      <div >
+      <img src={Logo3} width={"100px"}  />
+      </div>
+      </Link>
+       
+        </div>
+
+        <div style={{border:"1px solid red",display:"flex", marginLeft:"280px",gap:"50px", textDecoration:"none" }} >
+        <Link to={"/about"} style={{textDecoration:"none" , color:"black"}} >ABOUT</Link>
+        <Link to={"/men"}  style={{textDecoration:"none" ,  color:"black"}}  >MEN</Link>
+        <Link to={"/women"}  style={{textDecoration:"none",  color:"black"}}  >WOMEN</Link>
+        <Link to={"/shop"}  style={{textDecoration:"none",  color:"black" }}  >SHOP</Link>
+        <Link to={"/contact"}  style={{textDecoration:"none",  color:"black" }}  >CONTACT</Link>
+        </div> 
+
     </div>
   )
 }
