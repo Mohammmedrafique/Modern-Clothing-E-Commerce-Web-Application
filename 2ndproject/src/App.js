ca3459da7a665fd6d170f67722a539e27364e3e2
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-10-02 00:45:03 +0530

    d

--- 2ndproject/src/App.js
+++ 2ndproject/src/App.js
@@ -3,11 +3,11 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Shop from "./pages/Shop";
-import {Login} from "./pages/Login";
+import { Login } from "./pages/Login";
 
 import { productsData } from "./api/Api";
 import Product from "./components/Product";
-
+import Footer from "./components/Footer";
 import {
   createBrowserRouter,
   Outlet,
@@ -15,54 +15,52 @@ import {
   ScrollRestoration,
 } from "react-router-dom";
 
-const Layout=()=>{
+const Layout = () => {
   return (
     <div>
-      <Header/>
-      <ScrollRestoration/>
-      <Outlet/>
-      
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Layout/>,
-    children:[
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        path:"/",
-        element:<Home/>,
+        path: "/",
+        element: <Home />,
         loader: productsData,
-
       },
       {
-        path:"/product/:id",
-        element:<Product/>
-
+        path: "/product/:id",
+        element: <Product />,
       },
       {
-        path:"/cart",
-        element:<Cart/>,
+        path: "/cart",
+        element: <Cart />,
       },
       {
-        path:"/shop",
-        element:<Shop/>,
+        path: "/shop",
+        element: <Shop />,
         loader: productsData,
-
-      },{
-        path:"/login",
-        element:<Login/>
-      }
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
     ],
   },
-])
+]);
 
 const App = () => {
   return (
     <div className="font-bodyFont">
-     <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
   );
 };
--- 2ndproject/src/pages/Login.js
+++ 2ndproject/src/pages/Login.js
@@ -1,77 +1,102 @@
 import React from "react";
 import { Googlelogo, githublogo } from "../assets";
-import {GoogleAuthProvider,getAuth,signInWithPopup,signOut} from "firebase/auth"
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { ToastContainer, toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../redux/bazarSlice";
 import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  const auth=getAuth()
-  const provider=new GoogleAuthProvider();
-  const dispatch =useDispatch()
-  const navigate=useNavigate()
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleGoogleLogin=(e)=>{
-    e.preventDefault()
-    signInWithPopup(auth,provider).then((result)=>{
-      const user=result.user;
-      console.log(user)
-      dispatch(addUser({
-        _id:user.uid,
-        name:user.displayName,
-        email:user.email,
-        image:user.photoURL
+  const handleGoogleLogin = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        dispatch(
+          addUser({
+            _id: user.uid,
+            name: user.displayName,
+            email: user.email,
+            image: user.photoURL,
+          })
+        );
 
-      }))
+        setTimeout(() => {
+          navigate("/");
+        }, 1500);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-      setTimeout(()=>{
-        navigate('/')
-      },1500)
-    }).catch((error)=>{console.log(error)})
-
-  }
-
-  const handleSignOut=()=>{
+  const handleSignOut = () => {
     signOut(auth)
-    .then(()=>{
-      toast.success("Log Out Successfully!");
-      dispatch(removeUser())
-    })
-   
-    .catch((error)=>{
-      console.log(error);
-    })
-  }
+      .then(() => {
+        toast.success("Log Out Successfully!");
+        dispatch(removeUser());
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
-      <div className="w-full flex flex-col items-center justify-center gap-10">
-        <div onClick={handleGoogleLogin} className="text-base w-60 h-12 traking-wide border-[1px] border-gray-400 
-        rounded-md flex item-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
-          <img className="w-8" src={Googlelogo} alt="googlelogo" />
-          <span className="text-sm text-gray-900">Sign in with Google</span>
+      <div className="w-full flex flex-row items-center justify-center gap-10">
+        <div
+          onClick={handleGoogleLogin}
+          className="text-base  w-60 h-12 traking-wide border-[1px] border-gray-400 
+        rounded-md flex item-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300 p-1"
+        >
+          <img className="" src={Googlelogo} alt="googlelogo" />
+          <span className="text-md text-gray-900 flex items-center justify-center">
+            Sign in with Google
+          </span>
         </div>
-        <button onClick={handleSignOut} className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">Sign Out</button>
+        <button
+          onClick={handleSignOut}
+          className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
+        >
+          Sign Out
+        </button>
       </div>
-      <div className="w-full flex flex-col items-center justify-center gap-10">
-        <div   className="text-base w-60 h-12 traking-wide border-[1px] border-gray-400 
-        rounded-md flex item-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300">
-          <img className="w-8" src={githublogo} alt="githublogo" />
-          <span className="text-sm text-gray-900">Sign in with Github</span>
+      <div className="w-full flex flex-row items-center justify-center gap-10">
+        <div
+          className="text-base w-60 h-12 traking-wide border-[1px] border-gray-400 
+        rounded-md flex item-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300 p-2"
+        >
+          <img className="" src={githublogo} alt="githublogo" />
+          <span className="text-md text-gray-900 flex items-center justify-center">
+            Sign in with Github
+          </span>
         </div>
-        <button className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">Sign Out</button>
+        <button className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300">
+          Sign Out
+        </button>
       </div>
       <ToastContainer
-      position="top-left"
-      autoClose={2000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
       />
     </div>
   );
